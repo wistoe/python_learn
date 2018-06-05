924a4af7214a2990d1a12ae2c608463c5fb3c499
wisote <wistoe@163.com>
2018-06-05 18:06:39 +0800

    test1

---
+++ %.py
@@ -0,0 +1,10 @@
+print('%s is easy to learn'%'python')
+
+
+print("%s's score is %d"%('Mike',99))
+
+name='Mike'
+data=99
+print("%s's score is %d"%(name,data))
+
+
--- string.py
+++ string.py
@@ -13,5 +13,19 @@ print("""
 ****
 ***
 *
-"""
-      )
+""" )
+
+str1="I hate"
+str2=" you"
+print (str1+str2)
+
+print(str1+' and'+ str2)
+
+num=18
+str3="my age is "
+print("my age is "+str(num))
+print("my age is "+str(num))
+print("my age is %d" %num)
+
+
+
